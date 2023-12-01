eaa45fc87b2875e58b7b1315be2563f2b9c5add4
PraneethaMarthi <praneethamarthi@gmail.com>
2023-12-01 16:11:07 +1100

    Update UniqueBinarySearchTreeII.java

--- Leetcode - Java/LeetCodeJava/DAY6/UniqueBinarySearchTreeII.java
+++ Leetcode - Java/LeetCodeJava/DAY6/UniqueBinarySearchTreeII.java
@@ -1,14 +1,26 @@
 package DAY6;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-import javax.swing.tree.TreeNode;
+import java.util.Queue;
 
 //https://leetcode.com/problems/unique-binary-search-trees-ii/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
 public class UniqueBinarySearchTreeII {
-	public List<TreeNode> generateTrees(int n) {
-        if (n == 0) return new ArrayList<>();
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0)
+            return new ArrayList<>();
 
         List<TreeNode>[] dp = new ArrayList[n + 1];
         dp[0] = new ArrayList<>();
@@ -30,15 +42,49 @@ public class UniqueBinarySearchTreeII {
     }
 
     private TreeNode clone(TreeNode n, int offset) {
-        if (n == null) return null;
+        if (n == null)
+            return null;
         TreeNode node = new TreeNode(n.val + offset);
         node.left = clone(n.left, offset);
         node.right = clone(n.right, offset);
         return node;
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    public static void main(String[] args) {
+        UniqueBinarySearchTreeII uniqueBinarySearchTreeII = new UniqueBinarySearchTreeII();
 
+        int n = 3; // You can change this value as needed
+
+        List<TreeNode> result = uniqueBinarySearchTreeII.generateTrees(n);
+
+        // Display the generated trees in the specified format
+        System.out.println(generateTreesFormat(result));
+    }
+
+    // Helper function to print the tree structure in the specified format
+    private static String generateTreesFormat(List<TreeNode> trees) {
+        List<String> result = new ArrayList<>();
+        for (TreeNode root : trees) {
+            StringBuilder sb = new StringBuilder("[");
+            Queue<TreeNode> queue = new LinkedList<>();
+            if (root != null) {
+                queue.add(root);
+                while (!queue.isEmpty()) {
+                    TreeNode current = queue.poll();
+                    if (current != null) {
+                        sb.append(current.val);
+                        sb.append(",");
+                        queue.add(current.left);
+                        queue.add(current.right);
+                    } else {
+                        sb.append("null,");
+                    }
+                }
+                sb.deleteCharAt(sb.length() - 1); // Remove the trailing comma
+            }
+            sb.append("]");
+            result.add(sb.toString());
+        }
+        return result.toString();
+    }
 }
